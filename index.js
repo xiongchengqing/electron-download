690646dff14f2518b443f043cf081ba4aa6db9fc
adispring <sunnyadi@163.com>
2019-02-28 11:31:00 +0800

    showSaveDialog with customized filename

--- index.js
+++ index.js
@@ -1,146 +1,178 @@
-'use strict';
-const path = require('path');
-const {app, BrowserWindow, shell, dialog} = require('electron');
-const unusedFilename = require('unused-filename');
-const pupa = require('pupa');
-const extName = require('ext-name');
+const fs = require('fs')
+const path = require('path')
+const { app, BrowserWindow, shell, dialog } = require('electron')
+const pupa = require('pupa')
+const fetch = require('node-fetch')
+const os = require('os')
 
-function getFilenameFromMime(name, mime) {
-	const exts = extName.mime(mime);
+const isWindows = os.type() === 'Windows_NT'
 
-	if (exts.length !== 1) {
-		return name;
-	}
+function registerListener (session, options, cb = () => {}) {
+  const downloadItems = new Set()
+  let receivedBytes = 0
+  let completedBytes = 0
+  let totalBytes = 0
+  const activeDownloadItems = () => downloadItems.size
+  const progressDownloadItems = () => receivedBytes / totalBytes
 
-	return `${name}.${exts[0].ext}`;
+  options = Object.assign({ showBadge: true }, options)
+
+  const listener = (e, item, webContents) => {
+    downloadItems.add(item)
+    totalBytes += item.getTotalBytes()
+
+    let hostWebContents = webContents
+    if (webContents.getType() === 'webview') {
+      ;({ hostWebContents } = webContents)
+    }
+
+    const win = BrowserWindow.fromWebContents(hostWebContents)
+
+    const errorMessage =
+      options.errorMessage || 'The download of {filename} was interrupted'
+    const errorTitle = options.errorTitle || 'Download Error'
+
+    const dir = options.directory || app.getPath('downloads')
+    const { filePath } = options
+    item.setSavePath(filePath)
+
+    if (typeof options.onStarted === 'function') {
+      options.onStarted(item)
+    }
+
+    item.on('updated', () => {
+      receivedBytes = [...downloadItems].reduce((receivedBytes, item) => {
+        receivedBytes += item.getReceivedBytes()
+        return receivedBytes
+      }, completedBytes)
+
+      if (options.showBadge && ['darwin', 'linux'].includes(process.platform)) {
+        app.setBadgeCount(activeDownloadItems())
+      }
+
+      if (!win.isDestroyed()) {
+        win.setProgressBar(progressDownloadItems())
+      }
+
+      if (typeof options.onProgress === 'function') {
+        options.onProgress(progressDownloadItems())
+      }
+    })
+
+    item.on('done', (event, state) => {
+      completedBytes += item.getTotalBytes()
+      downloadItems.delete(item)
+
+      if (options.showBadge && ['darwin', 'linux'].includes(process.platform)) {
+        app.setBadgeCount(activeDownloadItems())
+      }
+
+      if (!win.isDestroyed() && !activeDownloadItems()) {
+        win.setProgressBar(-1)
+        receivedBytes = 0
+        completedBytes = 0
+        totalBytes = 0
+      }
+
+      if (options.unregisterWhenDone) {
+        session.removeListener('will-download', listener)
+      }
+
+      if (state === 'cancelled') {
+        if (typeof options.onCancel === 'function') {
+          options.onCancel(item)
+        }
+      } else if (state === 'interrupted') {
+        const message = pupa(errorMessage, { filename: item.getFilename() })
+        dialog.showErrorBox(errorTitle, message)
+        cb(new Error(message))
+      } else if (state === 'completed') {
+        if (process.platform === 'darwin') {
+          app.dock.downloadFinished(filePath)
+        }
+
+        if (options.openFolderWhenDone) {
+          shell.showItemInFolder(path.join(dir, item.getFilename()))
+        }
+
+        cb(null, item)
+      }
+    })
+  }
+
+  session.on('will-download', listener)
 }
 
-function registerListener(session, options, cb = () => {}) {
-	const downloadItems = new Set();
-	let receivedBytes = 0;
-	let completedBytes = 0;
-	let totalBytes = 0;
-	const activeDownloadItems = () => downloadItems.size;
-	const progressDownloadItems = () => receivedBytes / totalBytes;
+const downloadWeb = async (win, url, options) => {
+  const dir = options.directory || app.getPath('downloads')
+  const res = await fetch(url, { credentials: 'include' })
 
-	options = Object.assign({
-		showBadge: true
-	}, options);
+  // 根据fetch 结果得到后缀名
+  const contentType = res.headers.get('content-type')
+  const extMatch = /^image\/(\w+)/.exec(contentType)
+  const imageExt = (extMatch && extMatch[1]) || 'png'
+  const filename = `${options.filename}.${imageExt}`
+  const originFilePath = path.join(dir, filename)
 
-	const listener = (e, item, webContents) => {
-		downloadItems.add(item);
-		totalBytes += item.getTotalBytes();
+  const filePath = dialog.showSaveDialog(win, {
+    defaultPath: `${originFilePath}`
+  })
+  if (!filePath) return
+  return new Promise((resolve, reject) => {
+    options = Object.assign({}, options, {
+      unregisterWhenDone: true,
+      filePath
+    })
 
-		let hostWebContents = webContents;
-		if (webContents.getType() === 'webview') {
-			({hostWebContents} = webContents);
-		}
+    registerListener(win.webContents.session, options, (err, item) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(item)
+      }
+    })
 
-		const win = BrowserWindow.fromWebContents(hostWebContents);
-
-		const dir = options.directory || app.getPath('downloads');
-		let filePath;
-		if (options.filename) {
-			filePath = path.join(dir, options.filename);
-		} else {
-			const filename = item.getFilename();
-			const name = path.extname(filename) ? filename : getFilenameFromMime(filename, item.getMimeType());
-
-			filePath = unusedFilename.sync(path.join(dir, name));
-		}
-
-		const errorMessage = options.errorMessage || 'The download of {filename} was interrupted';
-		const errorTitle = options.errorTitle || 'Download Error';
-
-		if (!options.saveAs) {
-			item.setSavePath(filePath);
-		}
-
-		if (typeof options.onStarted === 'function') {
-			options.onStarted(item);
-		}
-
-		item.on('updated', () => {
-			receivedBytes = [...downloadItems].reduce((receivedBytes, item) => {
-				receivedBytes += item.getReceivedBytes();
-				return receivedBytes;
-			}, completedBytes);
-
-			if (options.showBadge && ['darwin', 'linux'].includes(process.platform)) {
-				app.setBadgeCount(activeDownloadItems());
-			}
-
-			if (!win.isDestroyed()) {
-				win.setProgressBar(progressDownloadItems());
-			}
-
-			if (typeof options.onProgress === 'function') {
-				options.onProgress(progressDownloadItems());
-			}
-		});
-
-		item.on('done', (event, state) => {
-			completedBytes += item.getTotalBytes();
-			downloadItems.delete(item);
-
-			if (options.showBadge && ['darwin', 'linux'].includes(process.platform)) {
-				app.setBadgeCount(activeDownloadItems());
-			}
-
-			if (!win.isDestroyed() && !activeDownloadItems()) {
-				win.setProgressBar(-1);
-				receivedBytes = 0;
-				completedBytes = 0;
-				totalBytes = 0;
-			}
-
-			if (options.unregisterWhenDone) {
-				session.removeListener('will-download', listener);
-			}
-
-			if (state === 'cancelled') {
-				if (typeof options.onCancel === 'function') {
-					options.onCancel(item);
-				}
-			} else if (state === 'interrupted') {
-				const message = pupa(errorMessage, {filename: item.getFilename()});
-				dialog.showErrorBox(errorTitle, message);
-				cb(new Error(message));
-			} else if (state === 'completed') {
-				if (process.platform === 'darwin') {
-					app.dock.downloadFinished(filePath);
-				}
-
-				if (options.openFolderWhenDone) {
-					shell.showItemInFolder(path.join(dir, item.getFilename()));
-				}
-
-				cb(null, item);
-			}
-		});
-	};
-
-	session.on('will-download', listener);
+    win.webContents.downloadURL(url)
+  })
 }
 
-module.exports = (options = {}) => {
-	app.on('session-created', session => {
-		registerListener(session, options);
-	});
-};
+// 下载本地图片
+const downloadLocal = (win, localPath, options) => {
+  const dir = options.directory || app.getPath('downloads')
+  localPath = decodeURIComponent(localPath)
+  const regex = /^file:\/\/\/(.*)$/
+  const m = regex.exec(localPath)
+  // 如果是file 协议, 就取其中的绝对路径, mac 前需要有 /
+  if (m) {
+    localPath = isWindows ? m[1] : `/${m[1]}`
+  }
+  try {
+    fs.accessSync(localPath, fs.constants.R_OK)
+  } catch (e) {
+    return
+  }
+  const imageExt = (path.extname(localPath) || '').slice(1)
+  const filename = `${options.filename}.${imageExt}`
+  const originFilePath = path.join(dir, filename)
+  const filePath = dialog.showSaveDialog(win, {
+    defaultPath: `${originFilePath}`
+  })
+  if (!filePath) return
 
-module.exports.default = module.exports;
+  try {
+    fs.createReadStream(localPath).pipe(fs.createWriteStream(filePath))
+  } catch (e) {
+    console.log(e)
+  }
+}
 
-module.exports.download = (win, url, options) => new Promise((resolve, reject) => {
-	options = Object.assign({}, options, {unregisterWhenDone: true});
-
-	registerListener(win.webContents.session, options, (err, item) => {
-		if (err) {
-			reject(err);
-		} else {
-			resolve(item);
-		}
-	});
-
-	win.webContents.downloadURL(url);
-});
+module.exports = (win, url, options) => {
+  if (
+    url.startsWith('http') ||
+    url.startsWith('https') ||
+    url.startsWith('data:')
+  ) {
+    downloadWeb(win, url, options)
+  }
+  downloadLocal(win, url, options)
+}
